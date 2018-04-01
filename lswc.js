a5ede220774df06ecfed71a127f6b03764853095
alexmoise <alex@moise.pro>
2018-04-01 21:39:11 +0300

    Adjusting the javascript events

--- lswc.js
+++ lswc.js
@@ -1,16 +1,29 @@
 
-// === PRODUCT OPTIONS SELECT CONDITIONALS - LET'S HAVE THIS MOVED TO A PLUGIN LATER ON ===
-// Submit the form (adding product to cart) when Payment Plan option is chosen
-
-jQuery( document ).ready( function () {
-	$('.table.variations input[name="attribute_pa_paying-plan"]').on('click', function() {
-	    $(this).closest("form").submit();
-	});
+// === Submit the form automatically (adding product to cart) when Payment Plan option is chosen
+jQuery( document ).delegate( '.table.variations input[name="attribute_pa_paying-plan"]', 'click', function(event) {
+	$(this).unbind('click');
+	$(this).closest("form").submit();
+});
+// === Toggle a class of parent DIV of radio buttons that ARE DISABLED or ARE SELECTED - these are styled further in CSS
+jQuery( document ).delegate( '#table-variations', 'change', function(event) {
+	$(this).unbind('click');
+	jQuery( 'input[disabled="disabled"]'		).parent('div').toggleClass('has-been-disabled',true);
+	jQuery( 'input:not([disabled="disabled"])'	).parent('div').toggleClass('has-been-disabled',false);
+	jQuery( 'input[type="radio"]:checked'		).parent('div').toggleClass('radio-checked',true);
+	jQuery( 'input[type="radio"]:not(:checked)'	).parent('div').toggleClass('radio-checked',false);
+	$('*[class=""]').removeAttr('class'); // removing empty "class" attribute, but only when it's empty ;-)
+});
+// === Toggle a class of parent DIV of disabled elements at first window.load anyway
+jQuery(window).load(function() {
+	jQuery( 'input[disabled="disabled"]' ).parent('div').toggleClass('has-been-disabled',true);
 });
 
 
-// Add a class to payment options when WIDTH is selected
 
+
+
+// === PRODUCT OPTIONS SELECT CONDITIONALS - FOR DECK 1 TEST ===
+// Add a class to payment options when WIDTH is selected
 jQuery( document ).ready( function () {
 	$('.table.variations input[name="attribute_pa_width"]').change(function(){
 	   if(this.checked) 
@@ -21,10 +34,7 @@ jQuery( document ).ready( function () {
 	   }
 	});
 });
-
-
 // Add a class to payment options when MODEL is selected
-
 jQuery( document ).ready( function () {
 	$('.table.variations input[name="attribute_pa_model"]').change(function(){
 	   if(this.checked) 
@@ -35,10 +45,7 @@ jQuery( document ).ready( function () {
 	   }
 	});
 });
-
-
 // Toggle a class of parent DIV of radio buttons - it's styled further in Additional CSS
-
 jQuery( document ).ready( function () {
 	$('div.attribute-pa_model.tr input[type="radio"]').on('change', function(){
 		$('input[type="radio"]').siblings().closest('div').removeClass('has-been-selected');
@@ -47,69 +54,29 @@ jQuery( document ).ready( function () {
 		   $(this).closest('div').addClass("has-been-selected", this.checked);
 	   } 
 	});
-});
+}); // === END FOR DECK 1 TEST ===
 
 
-// Toggle a class of parent DIV of radio buttons that ARE DISABLED or ARE SELECTED - these are styled further in Additional CSS
-
-jQuery( document ).delegate( '#table-variations', 'change', function(event) {
-	$(this).unbind('click');
-	
-	
-	if( ('.table.variations input[name="attribute_vert"]').checked)
-	{
-		$('.attribute-pa_paying-plan').addClass('unhide-by-width unhide-by-model');
-	}
-	
-	
-	
-	
-	jQuery( 'input[disabled="disabled"]' ).parent('div').toggleClass('has-been-disabled',true);
-	jQuery( 'input:not([disabled="disabled"])' ).parent('div').toggleClass('has-been-disabled',false);
-	jQuery( 'input[type="radio"]:checked' ).parent('div').toggleClass('radio-checked',true);
-	jQuery( 'input[type="radio"]:not(:checked)' ).parent('div').toggleClass('radio-checked',false);
-	$('*[class=""]').removeAttr('class'); // removing empty "class" attribute, but only when it's empty ;-)
-});
-
-
-// Toggle a class of parent DIV of disabled elements at first window.load anyway
-
-jQuery(window).load(function() {
-	jQuery( 'input[disabled="disabled"]' ).parent('div').toggleClass('has-been-disabled',true);
-});
-
-// === PRODUCT OPTIONS SELECT CONDITIONALS - END ===
-
-
-
-
-// === DOING IT AGAIN, FOR TEST 2 DECK ===
+// == DOING IT AGAIN, FOR ==TEST== 2 DECK ===
 // Add a class to payment options when WIDTH is selected
-/* 
-jQuery( document ).ready( function () {
-	$('.table.variations input[name="attribute_vert"]').change(function(){
-	   if(this.checked) 
-	   {
-		   $('.attribute-pa_paying-plan').addClass('unhide-by-width unhide-by-model');
-	   } else {
-		   $('.attribute-pa_paying-plan').removeClass('unhide-by-width unhide-by-model');
-	   }
-	});
+jQuery( document ).delegate( '.table.variations input[name="attribute_vert"]', 'change', function(event) {
+	$(this).unbind('click');
+	if(this.checked) 
+	{
+		$('.attribute-pa_paying-plan').toggleClass('unhide-by-width unhide-by-model',true);
+	} else {
+		$('.attribute-pa_paying-plan').toggleClass('unhide-by-width unhide-by-model',false);
+	}
 });
-*/ 
-
 // Add a class to payment options when MODEL is selected
-
-jQuery( document ).ready( function () {
-	$('.table.variations input[name="attribute_street"]').change(function(){
-	   if(this.checked) 
-	   {
-		   $('.attribute-pa_paying-plan').addClass('unhide-by-width unhide-by-model');
-	   } else {
-		   $('.attribute-pa_paying-plan').removeClass('unhide-by-width unhide-by-model');
-	   }
-	});
+jQuery( document ).delegate( '.table.variations input[name="attribute_street"]', 'change', function(event) {
+	$(this).unbind('click');
+	if(this.checked) 
+	{
+		$('.attribute-pa_paying-plan').toggleClass('unhide-by-width unhide-by-model',true);
+	} else {
+		$('.attribute-pa_paying-plan').toggleClass('unhide-by-width unhide-by-model',false);
+	}
 });
-
-// === PRODUCT OPTIONS SELECT CONDITIONALS ==TEST== END ===
+// == PRODUCT OPTIONS SELECT CONDITIONALS ==TEST== END ===
 
