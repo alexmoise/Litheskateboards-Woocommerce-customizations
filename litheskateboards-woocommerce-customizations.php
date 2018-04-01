10743a0778b1886d1206669fa30ddc62da344b9a
alexmoise <alex@moise.pro>
2018-04-01 19:14:54 +0300

    Added the variable.php Woocommerce template override

--- litheskateboards-woocommerce-customizations.php
+++ litheskateboards-woocommerce-customizations.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * Description: A custom plugin to add some JS, CSS and PHP functions for Woocommerce customizations. Main goals are: 1. have product options displayed as buttons in single product page, 2. have the last option show up only after selecting all previous ones, 3. jump directly to cart (checkout?) after selecting the last option. No settings page needed at this moment (but could be added later if needed). For details/troubleshooting please contact me at https://moise.pro/contact/
- * Version: 0.1.0
+ * Version: 0.1.4
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
 
 // Adding the JS
 function molswc_adding_scripts() {
-	wp_register_script('lswc-script', plugins_url('lswc.js', __FILE__), array('jquery'), true);
+	wp_register_script('lswc-script', plugins_url('lswc.js', __FILE__), array('jquery'), '', true);
 	wp_enqueue_script('lswc-script');
 }
 add_action( 'wp_enqueue_scripts', 'molswc_adding_scripts' ); 
@@ -23,5 +23,32 @@ function molswc_adding_styles() {
 	wp_register_style('lswc-styles', plugins_url('lswc.css', __FILE__));
 	wp_enqueue_style('lswc-styles');
 }
-add_action( 'wp_enqueue_scripts', 'molswc_adding_styles', 9999 );
+add_action( 'wp_enqueue_scripts', 'molswc_adding_styles', 9999999 ); // yeah, "avia-merged-styles" has 999999 :-P
+
+// Override WooCommerce Template and Parts
+add_filter( 'woocommerce_locate_template', 'molswc_replace_woocommerce_templates', 20, 3 );
+function molswc_replace_woocommerce_templates( $template, $template_name, $template_path ) {
+	global $woocommerce;
+	$_template = $template;
+	if ( ! $template_path ) { $template_path = $woocommerce->template_url; }
+	$plugin_path  = untrailingslashit( plugin_dir_path( __FILE__ ) )  . '/template/woocommerce/';
+	// Look within passed path within the theme - this is priority
+	$template = locate_template(
+		array(
+			$template_path . $template_name,
+			$template_name
+		)
+	);
+	if ( ! $template && file_exists( $plugin_path . $template_name ) ) { $template = $plugin_path . $template_name; }
+	if ( ! $template ) { $template = $_template; }
+	// echo '<br>Overr path: '.$plugin_path . $template_name.'<br>';
+	// echo '<br>_Template path: '.$_template.'<br>';
+	return $template;
+}
+
+
+
+
+
+
 
---
+++ template/woocommerce/single-product/add-to-cart/variable.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Variable product add to cart
+ *
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.5.0
+ *
+ * Modified to use radio buttons instead of dropdowns
+ * @author 8manos
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+global $product;
+
+$attribute_keys = array_keys( $attributes );
+
+do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+
+<form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo htmlspecialchars( wp_json_encode( $available_variations ) ) ?>">
+	<?php do_action( 'woocommerce_before_variations_form' ); ?>
+
+	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
+		<p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
+	<?php else : ?>
+	<p> EEEEEEE </p>
+		<div id="table-variations" class="table variations" cellspacing="0">
+			<div class="tbody">
+				<?php foreach ( $attributes as $name => $options ) : ?>
+					<div class="attribute-<?php echo sanitize_title($name); ?> tr">
+						<div class="label td"><label for="<?php echo sanitize_title( $name ); ?>"><?php echo wc_attribute_label( $name ); ?></label></div>
+						<?php
+						$sanitized_name = sanitize_title( $name );
+						if ( isset( $_REQUEST[ 'attribute_' . $sanitized_name ] ) ) {
+							$checked_value = $_REQUEST[ 'attribute_' . $sanitized_name ];
+						} elseif ( isset( $selected_attributes[ $sanitized_name ] ) ) {
+							$checked_value = $selected_attributes[ $sanitized_name ];
+						} else {
+							$checked_value = '';
+						}
+						?>
+						<div class="value td">
+							<?php
+							if ( ! empty( $options ) ) {
+								if ( taxonomy_exists( $name ) ) {
+									// Get terms if this is a taxonomy - ordered. We need the names too.
+									$terms = wc_get_product_terms( $product->get_id(), $name, array( 'fields' => 'all' ) );
+
+									foreach ( $terms as $term ) {
+										if ( ! in_array( $term->slug, $options ) ) {
+											continue;
+										}
+										print_attribute_radio( $checked_value, $term->slug, $term->name, $sanitized_name );
+									}
+								} else {
+									foreach ( $options as $option ) {
+										print_attribute_radio( $checked_value, $option, $option, $sanitized_name );
+									}
+								}
+							}
+
+							
+							?>
+						</div>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		</div>
+		<?php echo end( $attribute_keys ) === $name ? apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . __( 'Clear', 'woocommerce' ) . '</a>' ) : ''; ?>
+		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+
+		<div class="single_variation_wrap">
+			<?php
+				do_action( 'woocommerce_before_single_variation' );
+				do_action( 'woocommerce_single_variation' );
+				do_action( 'woocommerce_after_single_variation' );
+			?>
+		</div>
+
+		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+	<?php endif; ?>
+
+	<?php do_action( 'woocommerce_after_variations_form' ); ?>
+</form>
+
+<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
