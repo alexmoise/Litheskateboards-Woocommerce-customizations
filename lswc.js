645e89333c25759c262d28608af49b6d6c6d07f4
Alex Moise <alex@moise.pro>
2018-04-22 23:29:28 +0300

    Scroll to payment options when Model is selected

--- lswc.js
+++ lswc.js
@@ -12,26 +12,22 @@ jQuery( document ).delegate( '.table.variations', 'change', function(event) {
 	if ($(this).parents().find('input[type="radio"]').is(':checked')) // "payment plan" is hidden by default (in CSS), here add class to unhide it if any other attribute is selected
 	{
 		$('.attribute-pa_paying-plan').toggleClass('unhide-payments',true);
+		if ($(window).width() < 768) {
+			$('html,body').animate({scrollTop: $(".unhide-payments").offset().top - 20}); // here scroll down to payment options, now that these are un-hidden
+		}
 	} else {
 		$('.attribute-pa_paying-plan').toggleClass('unhide-payments',false);
 	}
 	$( '*[class=""]' ).removeAttr('class'); // removing empty "class" attribute, but only when it's empty ;-)
+});
+// === Scroll back up on Clear Selection
+jQuery('.reset_variations').click(function(){
+	if ($(window).width() < 768) {
+		$('html,body').animate({scrollTop: $(".product_title").offset().top - 20});
+	}
 });
 // === Submit the form automatically (adding product to cart) when Payment Plan option is chosen
 jQuery( document ).delegate( '.table.variations input[name="attribute_pa_paying-plan"]', 'click', function(event) {
 	$(this).unbind('click');
 	$(this).closest("form").submit();
 });
-// === Removing attributes "name" part
-/* === not used any more, moved to PHP
-var board_models = ['Street', 'Vert']; // will need to sync this with attributes from Woocommerce
-jQuery(window).on('load', function(a) {
-	for (var i = 0; i < board_models.length; i++) {
-		// alert ('hei: ' + board_models[i]); // checking the proper attribute address
-		$('.table.variations .value.td label').each( function(c) {
-			oriText = $(this).html();
-			newText = oriText.replace(board_models[i] + ' ','');
-			$(this).html(newText);
-		}); 
-	}
-});  */
