f791350a29096cb51c2b412c797d3962a53ed4aa
Alex Moise <alex@moise.pro>
2018-05-02 00:54:16 +0300

    Support for variation prices

--- litheskateboards-woocommerce-customizations.php
+++ litheskateboards-woocommerce-customizations.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * Description: A custom plugin to add some JS, CSS and PHP functions for Woocommerce customizations. Main goals are: 1. have product options displayed as buttons in single product page, 2. have the last option show up only after selecting all previous ones, 3. jump directly to cart (checkout?) after selecting the last option. No settings page needed at this moment (but could be added later if needed). For details/troubleshooting please contact me at https://moise.pro/contact/
- * Version: 0.1.18
+ * Version: 0.1.19
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -31,12 +31,6 @@ function molswc_remove_wcvarhints_js() {
     wp_deregister_script('wm_variation_price_hints_script');
 }
 add_action('wp_enqueue_scripts','molswc_remove_wcvarhints_js');
-// ... and add a new JS for Variations Price Hints, which we'll customize to work with buttons :-)
-function molswc_readd_wcvarhints_js() {
-	wp_register_script('wm_variation_price_hints_script', plugins_url('price-hints-script.js', __FILE__), array('jquery'), '', true);
-	wp_enqueue_script('wm_variation_price_hints_script');
-}
-add_action( 'wp_enqueue_scripts', 'molswc_readd_wcvarhints_js'); 
 
 // Override WooCommerce Template used in WC Variations Radio Buttons
 add_filter( 'woocommerce_locate_template', 'molswc_replace_woocommerce_templates', 20, 3 );
@@ -59,19 +53,6 @@ function molswc_replace_woocommerce_templates( $template, $template_name, $templ
 	return $template;
 }
 
-// Replace TAXONOMY TYPE variations buttons function of WC Variations Radio Buttons plugin, in order to add the *variation description*
-if ( ! function_exists( 'print_attribute_radio_tax' ) ) {
-	function print_attribute_radio_tax( $checked_value, $value, $label, $name, $attrib_description ) {
-		global $product;
-		$input_name = 'attribute_' . esc_attr( $name ) ;
-		$esc_value = esc_attr( $value );
-		$id = esc_attr( $name . '_v_' . $value . $product->get_id() ); //added product ID at the end of the name to target single products
-		$checked = checked( $checked_value, $value, false );
-		$filtered_label = apply_filters( 'woocommerce_variation_option_name', $label, esc_attr( $name ) );
-		printf( '<div class="select tax"><input type="radio" name="%1$s" value="%2$s" id="%3$s" %4$s /><label class="option tax" selected="" for="%3$s" data-text-b="%5$s">%5$s</label><span class="attrib-description">%6$s</span></div>', $input_name, $esc_value, $id, $checked, $filtered_label, $attrib_description );
-	}
-}
-
 // Replace ATTRIBUTE TYPE variations buttons function of WC Variations Radio Buttons plugin, in order to add the *class needed to hook the variation price hints* JS
 if ( ! function_exists( 'print_attribute_radio_attrib' ) ) {
 	function print_attribute_radio_attrib( $checked_value, $value, $label, $name ) {
@@ -81,7 +62,20 @@ if ( ! function_exists( 'print_attribute_radio_attrib' ) ) {
 		$id = esc_attr( $name . '_v_' . $value . $product->get_id() ); //added product ID at the end of the name to target single products
 		$checked = checked( $checked_value, $value, false );
 		$filtered_label = apply_filters( 'woocommerce_variation_option_name', $label, esc_attr( $name ) );
-		printf( '<div class="select attrib"><input type="radio" name="%1$s" value="%2$s" id="%3$s" %4$s /><label class="option attrib" selected="" for="%3$s" data-text-b="%5$s">%5$s</label></div>', $input_name, $esc_value, $id, $checked, $filtered_label );
+		printf( '<div class="attrib"><!-- %1$s %2$s %3$s %4$s %5$s --><input type="radio" name="%1$s" value="%2$s" id="%3$s" %4$s /><label class="attrib option" value="%2$s" for="%3$s" data-text-fullname="%2$s" data-text-b="%5$s">%5$s</label></div>', $input_name, $esc_value, $id, $checked, $filtered_label );
+	}
+}
+
+// Replace TAXONOMY TYPE variations buttons function of WC Variations Radio Buttons plugin, in order to add the *variation description*
+if ( ! function_exists( 'print_attribute_radio_tax' ) ) {
+	function print_attribute_radio_tax( $checked_value, $value, $label, $name, $attrib_description ) {
+		global $product;
+		$input_name = 'attribute_' . esc_attr( $name ) ;
+		$esc_value = esc_attr( $value );
+		$id = esc_attr( $name . '_v_' . $value . $product->get_id() ); //added product ID at the end of the name to target single products
+		$checked = checked( $checked_value, $value, false );
+		$filtered_label = apply_filters( 'woocommerce_variation_option_name', $label, esc_attr( $name ) );
+		printf( '<div class="tax"><!-- %1$s %2$s %3$s %4$s %5$s %6$s --><input type="radio" name="%1$s" value="%2$s" id="%3$s" %4$s /><label class="tax option" value="%2$s" for="%3$s" data-text-fullname="%5$s" data-text-b="%5$s">%5$s</label><span class="attrib-description">%6$s</span></div>', $input_name, $esc_value, $id, $checked, $filtered_label, $attrib_description );
 	}
 }
 
@@ -91,4 +85,3 @@ function molswc_move_product_description() {
 	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 	add_action( 'woocommerce_after_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 }
-
