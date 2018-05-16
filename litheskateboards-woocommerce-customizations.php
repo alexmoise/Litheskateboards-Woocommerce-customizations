abaec4d42fb3e0646d8785a46502e65866cf30ce
Alex Moise <alex@moise.pro>
2018-05-16 23:20:36 +0300

    Version bump for update push

--- litheskateboards-woocommerce-customizations.php
+++ litheskateboards-woocommerce-customizations.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * Description: A custom plugin to add some JS, CSS and PHP functions for Woocommerce customizations. Main goals are: 1. have product options displayed as buttons in single product page, 2. have the last option show up only after selecting all previous ones, 3. jump directly to cart (checkout?) after selecting the last option. No settings page needed at this moment (but could be added later if needed). For details/troubleshooting please contact me at https://moise.pro/contact/
- * Version: 0.1.24
+ * Version: 0.1.25
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -23,7 +23,7 @@ function molswc_adding_scripts() {
 	wp_register_script('lswc-script', plugins_url('lswc.js', __FILE__), array('jquery'), '', true);
 	wp_enqueue_script('lswc-script');
 }
-add_action( 'wp_enqueue_scripts', 'molswc_adding_scripts', 999 ); 
+add_action( 'wp_enqueue_scripts', 'molswc_adding_scripts', 9999999 ); 
 
 // get rid of original JS from WC Variations Price Hints ... (for good, we won't replace it anymore as all functions are now in lswc.js
 function molswc_remove_wcvarhints_js() {
