d3071b663ae426ba56236eafedb2fbee7e4dae29
Alex <alex@moise.pro>
2019-11-26 16:06:43 +0200

    Change "All Models" to "All Shapes" in product filter

--- litheskateboards-woocommerce-customizations.php
+++ litheskateboards-woocommerce-customizations.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * Description: A custom plugin to add some JS, CSS and PHP functions for Woocommerce customizations. Main goals are: 1. have product options displayed as buttons in product popup and in single product page, 2. have the last option (Payment Plan) show up only after selecting a Width corresponding to a Model, 3. jump directly to checkout after selecting the last option (Payment Plan). Works based on "Quick View WooCommerce" by XootiX for popup, on "WooCommerce Variation Price Hints" by Wisslogic for price calculations and also on "WC Variations Radio Buttons" for transforming selects into buttons. Also uses the "YITH Pre-Order for WooCommerce" plugin as a base plugin for handling the Pre Order functions. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.1.32
+ * Version: 1.1.33
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -449,7 +449,7 @@ function molswc_product_filters() {
 	echo '
 		<form id="product-filters" class="product-filters">
 			<select name="Models">
-			  <option value="" selected disabled hidden>All Models</option>';
+			  <option value="" selected disabled hidden>All Shapes</option>';
 			  foreach ( $chosen_attribs as $chosen_attrib ) { 
 				echo '<option value="'.$chosen_attrib.'" disabled="disabled" '; 
 				if ( strtolower($chosen_attrib) == $preselect_model) {echo 'selected';} 
