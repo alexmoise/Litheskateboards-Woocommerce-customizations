b0e240fac81ad216ad8ea50691d4eaf682210c26
Alex <alex@moise.pro>
2019-12-23 16:41:24 +0200

    Load variation price hints JS in all pages except products

--- litheskateboards-woocommerce-customizations.php
+++ litheskateboards-woocommerce-customizations.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/Litheskateboards-Woocommerce-customizations
  * Description: A custom plugin to add some JS, CSS and PHP functions for Woocommerce customizations. Main goals are: 1. have product options displayed as buttons in product popup and in single product page, 2. have the last option (Payment Plan) show up only after selecting a Width corresponding to a Model, 3. jump directly to checkout after selecting the last option (Payment Plan). Works based on "Quick View WooCommerce" by XootiX for popup, on "WooCommerce Variation Price Hints" by Wisslogic for price calculations and also on "WC Variations Radio Buttons" for transforming selects into buttons. Also uses the "YITH Pre-Order for WooCommerce" plugin as a base plugin for handling the Pre Order functions. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.3.1
+ * Version: 1.3.2
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -51,7 +51,7 @@ function molswc_debug_to_file($output_label, $something_to_output) {
 // Get rid of original JS from WC Variations Price Hints (all its functions are changed and present now in lswc.js)
 add_action('wp_print_scripts','molswc_remove_wcvarhints_js');
 function molswc_remove_wcvarhints_js() {
-	if ( !is_shop() ) { // we need it in shop because here there's no "current" product, unless popup pops up with one
+	if ( is_product() ) { // we need it in shop and categories because there's no "current" product in these pages, unless popup pops up with one
 		wp_dequeue_script('wm_variation_price_hints_script');
 		wp_deregister_script('wm_variation_price_hints_script');
 	}
